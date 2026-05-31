dd4d5ddfba6b82db722dc591f52e52631e7367f2
BadazZ89 <badazz89@users.noreply.github.com>
2026-05-31 11:54:27 +0200

    Fix: guard seccomp.filter_count for 4.14 in app_profile.c

--- drivers/ksu/policy/app_profile.c
+++ drivers/ksu/policy/app_profile.c
@@ -87,7 +87,9 @@ static void disable_seccomp(void)
 
     current->seccomp.mode = 0;
     current->seccomp.filter = NULL;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
     atomic_set(&current->seccomp.filter_count, 0);
+#endif
     spin_unlock_irq(&current->sighand->siglock);
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 11, 0)
