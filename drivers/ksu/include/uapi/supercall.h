da34c695d954f106e0ca8e04cd7fb3b1a18a246c
BadazZ89 <badazz89@users.noreply.github.com>
2026-05-31 13:16:11 +0200

    fixup: add struct and constant defs to uapi/supercall.h

--- drivers/ksu/include/uapi/supercall.h
+++ drivers/ksu/include/uapi/supercall.h
@@ -3,6 +3,7 @@
 
 #include <linux/ioctl.h>
 #include <linux/types.h>
+#include "app_profile.h"
 
 #define KSU_IOCTL_GRANT_ROOT _IOW('k', 1, __u32)
 #define KSU_IOCTL_GET_INFO _IOR('k', 2, __u32)
@@ -26,5 +27,111 @@
 #define KSU_IOCTL_ADD_TRY_UMOUNT _IOW('k', 20, __u32)
 #define KSU_IOCTL_SET_INIT_PGRP _IOW('k', 21, __u32)
 #define KSU_IOCTL_GET_SULOG_FD _IOR('k', 22, __u32)
+
+#define KSU_GET_INFO_FLAG_LKM       (1 << 0)
+#define KSU_GET_INFO_FLAG_MANAGER   (1 << 1)
+#define KSU_GET_INFO_FLAG_LATE_LOAD (1 << 2)
+#define KSU_GET_INFO_FLAG_PR_BUILD  (1 << 3)
+
+#define EVENT_POST_FS_DATA   1
+#define EVENT_BOOT_COMPLETED 2
+#define EVENT_MODULE_MOUNTED 3
+
+#define KSU_MARK_GET     0
+#define KSU_MARK_MARK    1
+#define KSU_MARK_UNMARK  2
+#define KSU_MARK_REFRESH 3
+
+#define KSU_UMOUNT_WIPE     0
+#define KSU_UMOUNT_ADD      1
+#define KSU_UMOUNT_DEL      2
+#define KSU_UMOUNT_GETSIZE  3
+#define KSU_UMOUNT_GETLIST  4
+
+struct ksu_get_info_cmd {
+    __u32 version;
+    __u32 flags;
+};
+
+struct ksu_report_event_cmd {
+    __u32 event;
+};
+
+struct ksu_set_sepolicy_cmd {
+    __u64 data;
+    __u64 data_len;
+};
+
+struct ksu_check_safemode_cmd {
+    __u32 in_safe_mode;
+};
+
+struct ksu_get_allow_list_cmd {
+    __u32 uids[128];
+    __u32 count;
+};
+
+struct ksu_new_get_allow_list_cmd {
+    __s32 count;
+    __s32 total_count;
+    __s32 uids[];
+};
+
+struct ksu_uid_granted_root_cmd {
+    __u32 uid;
+    __u32 granted;
+};
+
+struct ksu_uid_should_umount_cmd {
+    __u32 uid;
+    __u32 should_umount;
+};
+
+struct ksu_get_manager_appid_cmd {
+    __u32 appid;
+};
+
+struct ksu_get_app_profile_cmd {
+    struct app_profile profile;
+};
+
+struct ksu_set_app_profile_cmd {
+    struct app_profile profile;
+};
+
+struct ksu_get_feature_cmd {
+    __u32 feature_id;
+    __u32 value;
+    __u32 supported;
+};
+
+struct ksu_set_feature_cmd {
+    __u32 feature_id;
+    __u32 value;
+};
+
+struct ksu_get_wrapper_fd_cmd {
+    __s32 fd;
+};
+
+struct ksu_manage_mark_cmd {
+    __u32 operation;
+    __u32 pid;
+    __u32 result;
+};
+
+struct ksu_nuke_ext4_sysfs_cmd {
+    __u64 arg;
+};
+
+struct ksu_add_try_umount_cmd {
+    __u32 mode;
+    __u64 arg;
+    __u32 flags;
+};
+
+struct ksu_get_sulog_fd_cmd {
+    __u32 flags;
+};
 
 #endif
