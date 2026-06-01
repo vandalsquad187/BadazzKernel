a2bff086f533cf6d35695782070a7d8ba3c287df
BadazZ89 <vandalsquad187@gmail.com>
2026-06-01 22:07:54 +0200

    Build #78 fix: replace ksys_close with sys_close in supercall.c for 4.14

--- drivers/kernelsu/supercall/supercall.c
+++ drivers/kernelsu/supercall/supercall.c
@@ -83,7 +83,7 @@ static void ksu_install_fd_tw_func(struct callback_head *cb)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0)
         close_fd(fd);
 #else
-        ksys_close(fd);
+        sys_close(fd);
 #endif
     }
 
