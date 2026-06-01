4e1bd9b9907b9a8c664ec8763bb6b1a2fc3b741b
vandalsquad187 <vandalsquad187@users.noreply.github.com>
2026-06-02 01:21:29 +0200

    kernelsu: fix ni_syscall symbol name for 4.14 arm64

--- drivers/kernelsu/hook/arm64/syscall_hook.c
+++ drivers/kernelsu/hook/arm64/syscall_hook.c
@@ -125,7 +125,11 @@ static int __init ksu_find_ni_syscall_slots(int *out_slots, int max_slots)
     if (!ksu_syscall_table || max_slots <= 0)
         return 0;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
     ni_syscall = (unsigned long)ksu_resolve_symbol_for_functable_hook("__arm64_sys_ni_syscall");
+#else
+    ni_syscall = (unsigned long)ksu_resolve_symbol_for_functable_hook("sys_ni_syscall");
+#endif
 
     pr_info("sys_ni_syscall: 0x%lx\n", ni_syscall);
 
